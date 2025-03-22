c475f290975288e774c2dde1f03d910484c2bbf8
Pietruszko <gabriel.karalus98@gmail.com>
2025-03-22 21:29:21 +0100

    Made and implemented Player class which clears code a lot! Started working on Ball class which will be using sprite groups so i can render multiple objects

--- game.py
+++ game.py
@@ -8,12 +8,52 @@ clock = pygame.time.Clock()
 running = True
 dt = 0 #delta time
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() - 100)
-player_model = pygame.Rect(player_pos.x, player_pos.y, 200, 20)
-player_model.center = player_pos
-speed_y = 0
-speed_x = 0
-max_speed = 400
+class Ball:
+    def __init__(self, x, y, radius, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+
+    def draw(self):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+
+    def update(self):
+        if self.y >= screen.get_height():
+            self.y = screen.get_height() / 2
+            self.x = screen.get_width() / 2
+
+class Player:
+    def __init__(self, x, y, width, height, color, speed, max_speed):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.speed = speed
+        self.max_speed = max_speed
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.rect.center = (x, y)
+
+    def draw(self):
+        pygame.draw.rect(screen, self.color, self.rect)
+        
+
+    def update(self):
+        if self.rect.x < 0:
+            self.rect.x = 0
+        if self.rect.x > screen.get_width() - self.width:
+            self.rect.x = screen.get_width() - self.width
+        keys = pygame.key.get_pressed()
+        self.rect.x += self.speed * dt
+        if keys[pygame.K_a]:
+            self.speed = pygame.math.clamp(self.speed - 15, -self.max_speed, 0)
+        if keys[pygame.K_d]:
+            self.speed = pygame.math.clamp(self.speed + 15, 0, self.max_speed)
+        if not keys[pygame.K_a] and not keys[pygame.K_d]:
+            self.speed = 0
+
+
 circle_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 circle = pygame.Rect(circle_pos.x, circle_pos.y, 20, 20)
 circle_direction_y = 1
@@ -21,6 +61,9 @@ initial_circle_direction_x = random.randint(-100, 100)
 circle_direction_x = 0
 circle_speed = 400
 circle_angle = 90
+
+player = Player(screen.get_width() / 2, screen.get_height() - 100, 400, 10, "black", 0 , 500)
+
 
 while running:
     #polling for events
@@ -33,33 +76,13 @@ while running:
 
     #render game here
 
-    pygame.draw.rect(screen, "black", player_model) #draw a rect at player_pos
-    pygame.draw.circle(screen, "red", circle.center, circle.width/2) #draw a circle at circle_pos
+    player.update()
+    player.draw()
 
-    keys = pygame.key.get_pressed() #get a list of all keys that are pressed
-    player_model.y += speed_y * dt
-    player_model.x += speed_x * dt
-    if player_model.x < 0:
-        player_model.x = 0
-        speed_x = 0
-    if player_model.x > screen.get_width() - player_model.width:
-        player_model.x = screen.get_width() - player_model.width
-        speed_x = 0
-    if keys[pygame.K_w]:
-        speed_y = -200
-    if keys[pygame.K_s]:
-        speed_y = 200
-    if keys[pygame.K_a]:
-        speed_x = pygame.math.clamp(speed_x - 15, -max_speed, 0)
-    if keys[pygame.K_d]:
-        speed_x = pygame.math.clamp(speed_x + 15, 0, max_speed)
-    if not keys[pygame.K_w] and not keys[pygame.K_s]:
-        speed_y = 0
-    if not keys[pygame.K_a] and not keys[pygame.K_d]:
-        speed_x = 0
+    pygame.draw.circle(screen, "red", circle.center, circle.width/2) #draw a circle at circle_pos
     
     def circle_collision():
-        if pygame.Rect.colliderect(player_model, circle):
+        if pygame.Rect.colliderect(player.rect, circle):
             return True
         else:
             return False  
@@ -69,10 +92,8 @@ while running:
 
     if circle.x <= 0 or circle.x >= screen.get_width() - circle.width:
         circle_direction_x *= -1
-        #print(f"Hit wall, angle: {pygame.math.Vector2.angle_to(pygame.Vector2(circle_direction_x, circle_direction_y), pygame.Vector2(0, 0))}")
     if circle.y <= 0:
         circle_direction_y *= -1
-        #print(f"Hit ceiling, angle: {pygame.math.Vector2.angle_to(pygame.Vector2(circle_direction_x, circle_direction_y), pygame.Vector2(0, 0))}")
     if circle.y >= screen.get_height():
         circle_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
         circle = pygame.Rect(circle_pos.x, circle_pos.y, 20, 20)
@@ -81,13 +102,7 @@ while running:
 
     if circle_collision():
         circle_direction_y *= -1
-        print ((speed_x + max_speed)/(max_speed + max_speed))
-        print (pygame.math.lerp(-150, 150, (speed_x + max_speed)/(max_speed + max_speed)))
-        print (circle_angle)
-        circle_direction_x = circle_angle + 90 + (pygame.math.lerp(-150, 150, (speed_x + max_speed)/(max_speed + max_speed)))
-        print (circle_direction_x)
-        
-        #print(f"Hit player, angle: {pygame.math.Vector2.angle_to(pygame.Vector2(circle_direction_x, circle_direction_y), pygame.Vector2(0, 0))}")
+        circle_direction_x = circle_angle + 90 + (pygame.math.lerp(-150, 150, (player.speed + player.max_speed)/(player.max_speed + player.max_speed)))
 
     #render game here
 
